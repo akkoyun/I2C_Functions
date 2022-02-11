9804e77f76caed5aa5e022205264b2784bb72c81
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-02-11 11:29:45 +0300

    Initial update

---
+++ examples/test/test.ino
@@ -0,0 +1,20 @@
+#include "I2C_Functions.h"
+
+void setup() {
+  
+  // Serial Communication Start
+  Serial.begin(115200);
+  
+  // Header
+  Serial.println("       I2C Functions      ");
+  Serial.println("--------------------------");
+
+}
+
+void loop() {
+  
+ 
+  // Loop Delay
+  delay(1000);
+
+}
