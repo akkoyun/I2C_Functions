334458830a7959709a34cf6d5f347ab160e70292
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-02-25 09:04:44 +0300

    01.03.02 - Readme update

--- src/I2C_Sensor_Library.h
+++ src/I2C_Sensor_Library.h
@@ -8,6 +8,7 @@
 #define	__ADDR_HDC201__		(uint8_t)0x40
 #define	__ADDR_MAX17055__	(uint8_t)0x36
 #define	__ADDR_BQ24298__	(uint8_t)0x6B
+#define	__ADDR_SHT21__		(uint8_t)0x40
 
 enum Multiplexer_Channels {
 	__OFF__					= 0,
@@ -19,6 +20,13 @@ enum Multiplexer_Channels {
 	__CH6__					= 6,
 	__CH7__					= 7,
 	__CH8__					= 8
+};
+
+enum SHT21_Registers {
+	__SHT21_SOFT_RESET_		= 0xFE,
+	__SHT21_USER_REGISTER_	= 0xE6,
+	__SHT21_T_MEASUREMENT_	= 0xE3,
+	__SHT21_H_MEASUREMENT_	= 0xE5,
 };
 
 #endif
